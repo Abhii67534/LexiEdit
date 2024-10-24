739c675955e4e41cce20338014bebbc65fed99ce
Abhii67534 <abhisharma4950@gmail.com>
2024-10-24 10:57:25 -0400

    Added metadata

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
 'use client'
+import { Editor } from '@/components/editor/Editor'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-
+    <div>
+      <div className='text-color-white flex justify-center font-bold p-3 pb-0'>Lexical Editor</div>
+      <Editor/>
+    </div>
     </div>
   )
 }
